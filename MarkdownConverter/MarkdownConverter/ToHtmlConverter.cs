40a65705cd36d436439b1664910b272bde805fdf
Jonas Cardon <Jcardon@skynet.be>
2026-05-08 17:00:22 +0200

    Initial commit

---
+++ MarkdownConverter/MarkdownConverter/ToHtmlConverter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarkdownConverter
+{
+    public class ToHtmlConverter
+    {
+
+    }
+}
---
+++ MarkdownConverter/MarkdownConverterTests/MardownConverterTests.cs
@@ -0,0 +1,15 @@
+﻿namespace MarkdownConverterTests
+{
+    public class MardownConverterTests
+    {
+        /*
+         convert italic to <em>...</em> (happy and sad paths)
+         convert bold to <strong>...</strong> (happy and sad paths)
+         convert italic and bold to <strong><em>...</em></strong> (happy and sad paths)
+         convert paragraphs to <p>...</p> (happy and sad paths)
+         convert page breaks to <br> (happy and sad paths)
+         convert ordered lists to <ol><li>...</li></ol> (happy and sad paths)
+         convert unordered lists to <ul><li>...</li></ul> (happy and sad paths)
+         */
+    }
+}
